66df307a6dd836c430cfff56b81caf1680a2dc1c
Gaurav Kumar <gouravkumar2701@gmail.com>
2021-09-11 22:24:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const {ApolloServer} = require("apollo-server");
+const {typeDefs} = require("./Schema/type-defs");
+const {resolvers} = require("./Schema/resolvers");
+
+
+const server = new ApolloServer({ typeDefs, resolvers});
+
+
+server.listen().then(({url}) => {
+    console.log(`YOUR SERVER IS RUNNING AT: ${url} :`);
+});
+
+
