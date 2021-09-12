4de45043edba1e5e505849f5a80f0caf91bf0659
Gaurav Kumar <gouravkumar2701@gmail.com>
2021-09-12 18:04:59 +0530

    added more schema

--- Schema/type-defs.js
+++ Schema/type-defs.js
@@ -8,12 +8,22 @@ const typeDefs = gql`
         username: String!
         age: Int!
         nationality: String!
+        friends: [User]
     }
 
 
+    type Movie{
+        id: ID!
+        name: String!
+        yearOfPublication: Int!
+        isInTheaters: Boolean!
+    }
 
     type Query{
         users: [User!]!
+        user(id: ID!): User!
+        movie(id: ID!): Movie!
+        movies: [Movie!]!
     }
 
 `;
